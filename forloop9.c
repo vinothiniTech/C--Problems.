54113a0f5945cc5d4f7e53b7992f41587d8cacfe
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-01-28 21:19:40 +0530

    Add files via upload
    forloop9.c

---
+++ forloop9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int n,num,count=0;
+    scanf("%d",&num);
+    n=num;
+    while (n>=0) {
+        num=num/10;
+        count++;
+    }
+    printf("%d",count);
+    return 0;
