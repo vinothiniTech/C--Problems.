4e8ce8ed92ffb9b1be8e063c08bb4adcaf33015b
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-01-28 21:56:46 +0530

    Add files via upload
    forloop.c solved problem

---
+++ forloop11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    scanf("%d", &n);
+
+    for (; n > 0; n = n / 10) {
+        sum = sum + (n % 10);
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
