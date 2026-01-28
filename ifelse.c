8170ce6bab34e918cab84be3b47b285f11f6a79f
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-01-28 16:34:39 +0530

    Add files via upload
    solved the if else problem

---
+++ ifelse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n>1) {
+        printf("%d is a positive number");
+    }
+    else{
+        printf("%d is a negative number");
+    }
+    return 0;
+}
