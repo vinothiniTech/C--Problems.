7d7c4ff7e36555532c81ffbc365082740279acc3
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-01-29 11:51:12 +0530

    Add files via upload

---
+++ pattern7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++) {
+        for (int s=1;s<=n-i;s++) {
+            printf(" ");
+        }
+        for (int j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+        return 0;
+    }
