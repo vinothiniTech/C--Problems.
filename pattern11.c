1ba2b3ba4dc3b6ba3535c7c05de7c81660ddd983
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-01-29 15:25:46 +0530

    Add files via upload

---
+++ pattern11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++) {
+        for (int j=i;j>=1;j--) {
+            if (j%2==0) {
+                printf("0");
+            }
+            else
+                printf("1");
+        }
+        printf("\n");
+    }
+    return 0;
+}
