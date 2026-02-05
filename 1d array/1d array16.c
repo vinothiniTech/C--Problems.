c9d3e99dd608cac0e2729555a9ea9db216a1afc9
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-02-05 09:23:51 +0530

    Add files via upload

---
+++ 1d array/1d array16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int n,a[100],i,min,profit;
+    scanf("%d",&n);
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+
+    min = a[0];
+    profit = a[1]-a[0];
+
+    for(i=1;i<n;i++){
+        if(a[i]-min > profit)
+            profit = a[i]-min;
+        if(a[i] < min)
+            min = a[i];
+    }
+
+    printf("%d",profit);
+    return 0;
+}
