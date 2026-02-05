9637b7cce7134ff7b9abb7bee703721335c56da7
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-02-05 09:18:40 +0530

    Add files via upload

---
+++ 1d array/1d array7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n,a[100],i,even=0,odd=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        if(a[i]%2==0) even++;
+        else odd++;
+    }
+    printf("Even:%d Odd:%d",even,odd);
+    return 0;
+}
