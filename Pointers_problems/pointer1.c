d0a924b778ed199cd1be55418ebd3161262f7e74
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-02-12 14:30:02 +0530

    Update pointer1.c

--- Pointers_problems/pointer1.c
+++ Pointers_problems/pointer1.c
@@ -4,12 +4,13 @@ void swap(int a,int b) {
     a=b;
     b=temp;
     printf("swap is done\n",a,b);
+    printf("after swap %d %d\n",a,b);
 }
 int main() {
     int a,b;
     scanf("%d %d",&a,&b);
     printf("before swap %d %d\n",a,b);
     swap(a,b);
-    printf("after swap %d %d\n",a,b);
+    
     return 0;
 }
