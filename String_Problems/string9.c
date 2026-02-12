5df0330f48889f11d400e01177319e2146542dba
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-02-12 11:54:31 +0530

    Add files via upload

---
+++ String_Problems/string9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+
+    fgets(str, sizeof(str), stdin);
+
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+    }
+
+    printf("%s", str);
+    return 0;
+}
+
