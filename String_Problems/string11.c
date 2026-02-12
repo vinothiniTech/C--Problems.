ac8429d748ee8279050a1a6ccb31e03f0cdde41e
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-02-12 11:55:17 +0530

    Add files via upload

---
+++ String_Problems/string11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+
+    scanf("%s", str);
+
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] >= '0' && str[i] <= '9')
+            count++;
+    }
+
+    printf("%d", count);
+    return 0;
+}
