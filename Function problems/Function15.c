a36f3572a03aa8c0733ef63530f29bde1751d3cb
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-02-15 22:17:58 +0530

    Add files via upload

---
+++ Function problems/Function15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void table(int n) {
+    for(int i = 1; i <= 10; i++)
+        printf("%d×%d=%d\n", n, i, n*i);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    table(n);
+    return 0;
+}
