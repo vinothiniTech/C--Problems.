dfc56b9416a2b0241c0b490606263f883364ca46
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-02-16 13:20:53 +0530

    Add files via upload

---
+++ Pointers_problems/Pointer8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    char str1[100], str2[100];
+    fgets(str1, sizeof(str1), stdin);
+    fgets(str2, sizeof(str2), stdin);
+
+    char *p1 = str1;
+    char *p2 = str2;
+
+    while(*p1 != '\0')
+        p1++;
+
+    p1--;  // remove newline
+
+    while(*p2 != '\0') {
+        *p1 = *p2;
+        p1++;
+        p2++;
+    }
+
+    *p1 = '\0';
+
+    printf("%s", str1);
+
+    return 0;
+}
